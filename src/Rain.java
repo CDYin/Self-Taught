ee3f80850b0640723cea9b0c732fee50254ca0f0
CDYin <1157878136@qq.com>
2021-11-03 17:44:23 +0800

    One question per day

---
+++ src/Rain.java
@@ -0,0 +1,51 @@
+import java.util.PriorityQueue;
+
+/**
+ * 给你一个 m x n 的矩阵，其中的值均为非负整数，代表二维高度图每个单元的高度，请计算图中形状最多能接多少体积的雨水。
+ *
+ * 这个题，我能做到只考虑到最外层，然后内层也需要考虑，直接gg，CtrlC，V提交，毕竟困难题，给一些尊重
+ **/
+
+public class Rain {
+    public int trapRainWater(int[][] heightMap) {
+        if (heightMap.length <= 2 || heightMap[0].length <= 2) {
+            return 0;
+        }
+        int m = heightMap.length;
+        int n = heightMap[0].length;
+        boolean[][] visit = new boolean[m][n];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == 0 || i == m - 1 || j == 0 || j == n - 1) {
+                    pq.offer(new int[]{i * n + j, heightMap[i][j]});
+                    visit[i][j] = true;
+                }
+            }
+        }
+        int res = 0;
+        int[] dirs = {-1, 0, 1, 0, -1};
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            for (int k = 0; k < 4; ++k) {
+                int nx = curr[0] / n + dirs[k];
+                int ny = curr[0] % n + dirs[k + 1];
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n && !visit[nx][ny]) {
+                    if (curr[1] > heightMap[nx][ny]) {
+                        res += curr[1] - heightMap[nx][ny];
+                    }
+                    pq.offer(new int[]{nx * n + ny, Math.max(heightMap[nx][ny], curr[1])});
+                    visit[nx][ny] = true;
+                }
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int [][] heightMap = {{12,13,1,12},{13,4,13,12},{13,8,10,12},{12,13,12,12},{13,13,13,13}};
+        Rain rain = new Rain();
+        int result = rain.trapRainWater(heightMap);
+        System.out.println(result);
+    }
+}
